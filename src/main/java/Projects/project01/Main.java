6abee90c3e3125809d00aaf7fd2a04529ba734d4
Lyhong9 <activatorcode9999@gmail.com>
2026-06-20 13:02:23 +0700

    Updated Project input and display dynamic

--- src/main/java/Projects/project01/Main.java
+++ src/main/java/Projects/project01/Main.java
@@ -6,21 +6,43 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Staff[] staffs = new Staff[500];
-        int n, i;
-        System.out.print("PLEASE INPUT Number OF STAFF: ");
-        n = input.nextInt();
-        for (i = 0; i < n; i++) {
-            System.out.println("=====================================");
-            System.out.println("         INPUT "+ n + " STAFF DATA           ");
-            System.out.println("=====================================");
-            staffs[i] = new Staff();
-            staffs[i].input();
-        }
-        System.out.println("=====================================");
-        System.out.println("          STAFF INFORMATION          ");
-        System.out.println("=====================================");
-        for (i = 0; i < n; i++) {
-            staffs[i].display();
-        }
+        int n = 0, i, option;
+
+        do {
+            System.out.println("========== MENU ==========");
+            System.out.println("1. Add Staff");
+            System.out.println("2. Show Staff");
+            System.out.println("3. Exit Program");
+            System.out.println("==========================");
+            System.out.print("Input option (1-3): ");option = input.nextInt();
+            switch (option ) {
+                case 1:
+                    staffs[n] = new Staff();
+                    staffs[n].input();
+                    n++;
+                    break;
+                case 2:
+                    for (i = 0; i < n; i++) {
+                        staffs[i].display();
+                    }
+                    break;
+            }
+        }while (option != 3);
+
+//        System.out.print("PLEASE INPUT Number OF STAFF: ");
+//        n = input.nextInt();
+//        for (i = 0; i < n; i++) {
+//            System.out.println("=====================================");
+//            System.out.println("         INPUT "+ n + " STAFF DATA           ");
+//            System.out.println("=====================================");
+//            staffs[i] = new Staff();
+//            staffs[i].input();
+//        }
+//        System.out.println("=====================================");
+//        System.out.println("          STAFF INFORMATION          ");
+//        System.out.println("=====================================");
+//        for (i = 0; i < n; i++) {
+//            staffs[i].display();
+//        }
     }
 }
