76fcd79edb23090c733aa0abb8d3138838525b9c
Lyhong9 <activatorcode9999@gmail.com>
2026-06-23 15:00:38 +0700

    Updated Project Search Staff ID

--- src/main/java/Projects/project01/Main.java
+++ src/main/java/Projects/project01/Main.java
@@ -7,12 +7,13 @@ public class Main {
         Scanner input = new Scanner(System.in);
         Staff[] staffs = new Staff[500];
         int n = 0, i, option;
-
+        double isSearch;
         do {
             System.out.println("========== MENU ==========");
             System.out.println("1. Add Staff");
             System.out.println("2. Show Staff");
-            System.out.println("3. Exit Program");
+            System.out.println("3. Search Staff");
+            System.out.println("4. Exit Program");
             System.out.println("==========================");
             System.out.print("Input option (1-3): ");option = input.nextInt();
             switch (option ) {
@@ -26,8 +27,24 @@ public class Main {
                         staffs[i].display();
                     }
                     break;
+                case 3:
+                    System.out.println("========== Search Staff ==========");
+                    System.out.print("Enter Staff ID: ");isSearch = input.nextDouble();
+                    int isFound = 0;
+                    for (i = 0; i < n; i++) {
+                        if (staffs[i].getId() == isSearch) {
+                            System.out.println("========== Data Found ==========");
+                            staffs[i].display();
+                            isFound = 1;
+                            break;
+                        }
+                    }
+                    if (isFound == 0) {
+                        System.out.println("========== Staff Not Found ==========");
+                    }
+                    break;
             }
-        }while (option != 3);
+        }while (option != 4);
 
 //        System.out.print("PLEASE INPUT Number OF STAFF: ");
 //        n = input.nextInt();
